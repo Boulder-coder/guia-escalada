b15d58d503c6d482a9c39b35a044a1c8c86106ee
Boulder-coder <boret.albert@gmail.com>
2026-06-26 11:11:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js: Precarga completa con soporte offline para SVGs con timestamp
-const CACHE_NAME = 'guia-escalada-v2';
+const CACHE_NAME = 'guia-escalada-v3';
 const DATA_JSON_URL = '/guia-escalada/datos/datos.json';
 
 // Normaliza URLs eliminando parámetros de timestamp
